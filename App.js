4f941f7ac9c69fe0e0e03e251523d9c75815c326
Jose Noel C. Gamba <joelgamba04@gmail.com>
2025-01-23 10:02:49 +0800

    Added buggy webrtc implementation

--- App.js
+++ App.js
@@ -1,43 +1,283 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Button, View } from "react-native";
-import { RTCView, mediaDevices } from "react-native-webrtc";
+import React, { useEffect, useRef, useState } from "react";
+import {
+  View,
+  Button,
+  StyleSheet,
+  LogBox,
+  PermissionsAndroid,
+  Platform,
+} from "react-native";
 
-import React, { useRef, useState } from "react";
+import {
+  RTCView,
+  mediaDevices,
+  RTCPeerConnection,
+  RTCIceCandidate,
+} from "react-native-webrtc";
+
+import io from "socket.io-client";
+
+const SERVER_URL = "http://10.0.48.42:5000";
+
+// Ignore warnings (use sparingly)
+LogBox.ignoreLogs(["Warning: ..."]);
+
+// Add global error handler
+const globalErrorHandler = (e, isFatal) => {
+  console.error("Global Error:", e, isFatal);
+};
+if (global.ErrorUtils) {
+  global.ErrorUtils.setGlobalHandler(globalErrorHandler);
+}
+
+const requestPermissions = async () => {
+  if (Platform.OS === "android") {
+    try {
+      const granted = await PermissionsAndroid.requestMultiple([
+        PermissionsAndroid.PERMISSIONS.CAMERA,
+        PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+      ]);
+
+      if (
+        granted[PermissionsAndroid.PERMISSIONS.CAMERA] !==
+          PermissionsAndroid.RESULTS.GRANTED ||
+        granted[PermissionsAndroid.PERMISSIONS.RECORD_AUDIO] !==
+          PermissionsAndroid.RESULTS.GRANTED
+      ) {
+        console.error("Permissions not granted");
+        return false;
+      }
+    } catch (error) {
+      console.error("Error requesting permissions:", error);
+      return false;
+    }
+  }
+  return true;
+};
 
 export default function App() {
-  const [streamRef, setStreamRef] = useState(null);
+  const [socketId, setSocketId] = useState(null);
+  const [stream, setStream] = useState(null);
+  const [remoteStream, setRemoteStream] = useState(null);
 
-  const startStream = async () => {
-    const stream = await mediaDevices.getUserMedia({
-      video: true,
-      audio: true,
+  const socket = useRef(null);
+  const peerConnection = useRef(null);
+
+  useEffect(() => {
+    // Connect to the signaling server
+    socket.current = io(SERVER_URL);
+
+    socket.current.on("connect", () => {
+      console.log("Connected to signaling server:", socket.current.id);
+      setSocketId(socket.current.id);
     });
 
-    console.log("started stream", stream);
-    setStreamRef(stream);
-    console.log("startStream streamRef", streamRef);
+    socket.current.on("callAllUsers", (data) => {
+      console.log("Received callAllUsers:", data);
+      handleIncomingCall(data);
+    });
+
+    socket.current.on("callAccepted", (signal) => {
+      console.log("Call accepted:", signal);
+      handleCallAccepted(signal);
+    });
+
+    socket.current.on("receiveIceCandidate", (data) => {
+      console.log("Received ICE candidate:", data);
+      handleIceCandidate(data);
+    });
+
+    return () => {
+      socket.current.disconnect();
+    };
+  }, []);
+
+  const startLocalStream = async () => {
+    console.log("startLocalStream()");
+    const hasPermissions = await requestPermissions();
+    if (!hasPermissions) {
+      console.error("Permissions not granted");
+      return;
+    }
+
+    try {
+      const localStream = await mediaDevices.getUserMedia({
+        video: true,
+        audio: true,
+      });
+      setStream(localStream);
+    } catch (error) {
+      console.error("Error accessing media devices:", error);
+    }
   };
 
+  const callAllUsers = () => {
+    console.log("callAllUsers() start");
+    try {
+      if (!stream) {
+        console.error("callAllUsers Local stream is not available");
+        return;
+      } else {
+        console.log("callAllUsers Local stream is available");
+      }
+
+      // close peer connection before creating a new one
+      if (peerConnection.current) {
+        console.log("close peer connection before creating a new one");
+        peerConnection.current.close();
+      }
+      peerConnection.current = new RTCPeerConnection();
+
+      // Add local tracks to the connection
+      stream.getTracks().forEach((track) => {
+        try {
+          peerConnection.current.addTrack(track, stream);
+          console.log("Adding local track:", track);
+        } catch (error) {
+          console.error("Error adding track:", error);
+        }
+      });
+
+      // Create offer
+      peerConnection.current.createOffer().then((offer) => {
+        console.log("Created offer:");
+
+        try {
+          peerConnection.current.setLocalDescription(offer);
+          socket.current.emit("callAllUsers", {
+            from: socketId,
+            signalData: offer,
+            name: "React Native User",
+          });
+        } catch (error) {
+          console.error("Error setting local description:", error);
+        }
+      });
+
+      // Listen for ICE candidates
+      peerConnection.current.onicecandidate = (event) => {
+        console.log("ICE candidate event:", event);
+        if (event.candidate) {
+          console.log("Sending ICE candidate:", event.candidate);
+
+          socket.current.emit("sendIceCandidate", {
+            to: socketId,
+            candidate: event.candidate,
+          });
+        }
+      };
+
+      // Handle remote stream
+      peerConnection.current.ontrack = (event) => {
+        try {
+          console.log("Received remote stream:", event.streams[0]);
+          setRemoteStream(event.streams[0]);
+        } catch (error) {
+          console.error("Error receiving remote stream:", error);
+        }
+      };
+    } catch (error) {
+      console.error("Error calling all users:", error);
+    }
+  };
+
+  const handleIncomingCall = (data) => {
+    console.log("handleIncomingCall() start");
+    try {
+      if (!stream) {
+        console.error("handleIncomingCall: Local stream is not available");
+        return;
+      }
+
+      if (peerConnection.current) {
+        console.log("close peer connection before creating a new one");
+        peerConnection.current.close();
+      }
+
+      peerConnection.current = new RTCPeerConnection();
+
+      // Add local tracks
+      stream.getTracks().forEach((track) => {
+        try {
+          peerConnection.current.addTrack(track, stream);
+          console.log("handleIncomingCall Added local track:", track);
+        } catch (error) {
+          console.error("Error adding track:", error);
+        }
+      });
+
+      console.log("Remote signal data:", data.signalData);
+
+      // Set remote description
+      peerConnection.current.setRemoteDescription(
+        new RTCSessionDescription(data.signalData)
+      );
+
+      // Create answer
+      peerConnection.current.createAnswer().then((answer) => {
+        try {
+          peerConnection.current.setLocalDescription(answer);
+
+          // Send answer back
+          socket.current.emit("answerCall", {
+            to: data.from,
+            signal: answer,
+          });
+        } catch (error) {
+          console.log("setLocalDescription", error);
+        }
+      });
+
+      // Handle remote stream
+      peerConnection.current.ontrack = (event) => {
+        try {
+          console.log("Received remote stream:", event.streams[0]);
+          setRemoteStream(event.streams[0]);
+        } catch (error) {
+          console.error("Error receiving remote stream:", error);
+        }
+      };
+    } catch (error) {
+      console.error("Error handling incoming call:", error);
+    }
+  };
+
+  const handleCallAccepted = (signal) => {
+    peerConnection.current.setRemoteDescription(
+      new RTCSessionDescription(signal)
+    );
+  };
+
+  const handleIceCandidate = ({ candidate, from }) => {
+    if (candidate) {
+      try {
+        peerConnection.current.addIceCandidate(new RTCIceCandidate(candidate));
+        console.log("ICE candidate added:", candidate);
+      } catch (error) {
+        console.error("Error adding ICE candidate:", error);
+      }
+    }
+  };
   return (
     <View style={styles.container}>
-      <RTCView
-        streamURL={streamRef ? streamRef.toURL() : ""}
-        style={styles.video}
-      />
-      <Button title="Start Stream" onPress={startStream} />
+      <View style={styles.videoContainer}>
+        {stream && <RTCView streamURL={stream.toURL()} style={styles.video} />}
+        {remoteStream && (
+          <RTCView streamURL={remoteStream.toURL()} style={styles.video} />
+        )}
+      </View>
+      <Button title="Start Local Stream" onPress={startLocalStream} />
+      <Button title="Call All Users" onPress={callAllUsers} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+  container: { flex: 1, justifyContent: "center", alignItems: "center" },
+  videoContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginBottom: 20,
   },
-  video: {
-    width: "100%",
-    height: "70%",
-    backgroundColor: "black",
-  },
+  video: { width: 150, height: 200, backgroundColor: "black" },
 });
